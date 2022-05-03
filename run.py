f3ea982062c0fe52af92ef8847bfcc8764d7bc59
Gurjit Singh <g.s@outlook.in>
2022-05-03 22:36:44 +0530

    tst

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from subprocess import run
+from shutil import which
 
-# run(r'@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"')
+(?# run(r'@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"'))
 
 if which("choco"):
     print('yoyo')
