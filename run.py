0f54c488f4ef2b515e23341b6f97d2f9477010c4
Gurjit Singh <g.s@outlook.in>
2022-05-03 22:31:51 +0530

    tst

---
+++ run.py
@@ -0,0 +1,6 @@
+from subprocess import run
+
+# run(r'@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"')
+
+if which("choco"):
+    print('yoyo')
